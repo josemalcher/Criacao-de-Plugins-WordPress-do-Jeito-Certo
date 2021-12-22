b206dee1a55c2021a4b501f327fa62909a8f6ec2
José Malcher Jr <contato@josemalcher.net>
2021-12-21 22:47:50 -0300

    44 API Shortcode - Tornando algumas opções dinâmicas (parte 1)

--- wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -1,5 +1,5 @@
 <h3><?php echo ( ! empty ( $content ) ) ? esc_html( $content ) : esc_html( MV_Slider_Settings::$options['mv_slider_title'] ); ?></h3>
-<div class="mv-slider flexslider ">
+<div class="mv-slider flexslider <?php echo ( isset( MV_Slider_Settings::$options['mv_slider_style'] ) ) ? esc_attr( MV_Slider_Settings::$options['mv_slider_style'] ) : 'style-1'; ?>">
     <ul class="slides">
 		<?php
 
