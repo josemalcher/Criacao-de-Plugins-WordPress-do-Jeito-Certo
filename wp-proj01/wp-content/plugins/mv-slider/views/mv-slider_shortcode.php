90a57e9d9fa00b198311ba4b71fa7cbcabd4c6ae
José Malcher Jr <contato@josemalcher.net>
2021-12-21 22:20:53 -0300

    41 API Shortcode - Criando a view do shortcode (parte 3)

--- wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -12,21 +12,34 @@
 
 		$my_query = new WP_Query( $args );
 
-		?>
-        <li>
-            <div class="mvs-container">
-                <div class="slider-details-container">
-                    <div class="wrapper">
-                        <div class="slider-title">
-                            <h2>Slider Title</h2>
-                        </div>
-                        <div class="slider-description">
-                            <div class="subtitle">Subtitle</div>
-                            <a class="link" href="#">Button text</a>
+		if ( $my_query->have_posts() ):
+			while ( $my_query->have_posts() ) : $my_query->the_post();
+
+				$button_text = get_post_meta( get_the_ID(), 'mv_slider_link_text', true );
+				$button_url  = get_post_meta( get_the_ID(), 'mv_slider_link_url', true );
+
+				?>
+                <li>
+					<?php the_post_thumbnail( 'full', array( 'class' => 'img-fluid' ) ); ?>
+                    <div class="mvs-container">
+                        <div class="slider-details-container">
+                            <div class="wrapper">
+                                <div class="slider-title">
+                                    <h2><?php the_title(); ?></h2>
+                                </div>
+                                <div class="slider-description">
+                                    <div class="subtitle"><?php the_content(); ?></div>
+                                    <a class="link"
+                                       href="<?php echo esc_attr( $button_url ); ?>"><?php echo esc_html( $button_text ); ?></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </li>
+                </li>
+			<?php
+			endwhile;
+			wp_reset_postdata();
+		endif;
+		?>
     </ul>
 </div>
