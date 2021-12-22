0e957041cba75f87766df68ec7b31905d959ac11
José Malcher Jr <contato@josemalcher.net>
2021-12-21 22:16:32 -0300

    43 API Shortcode - Registrando e enfileirando scripts (parte 2)

--- wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-proj01/wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -1,6 +1,18 @@
 <h3><?php echo ( ! empty ( $content ) ) ? esc_html( $content ) : esc_html( MV_Slider_Settings::$options['mv_slider_title'] ); ?></h3>
 <div class="mv-slider flexslider ">
     <ul class="slides">
+		<?php
+
+		$args = array(
+			'post_type'   => 'mv-slider',
+			'post_status' => 'publish',
+			'post__in'    => $id,
+			'orderby'     => $orderby
+		);
+
+		$my_query = new WP_Query( $args );
+
+		?>
         <li>
             <div class="mvs-container">
                 <div class="slider-details-container">
