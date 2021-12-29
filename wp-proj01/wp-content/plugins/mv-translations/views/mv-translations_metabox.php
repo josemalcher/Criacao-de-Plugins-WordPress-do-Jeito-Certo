37d96634411c6058fb676656e49757f49a838d8f
José Malcher Jr <contato@josemalcher.net>
2021-12-29 07:07:14 -0300

    77 Classe $wpdb - Selecionando dados com o método get_results

--- wp-proj01/wp-content/plugins/mv-translations/views/mv-translations_metabox.php
+++ wp-proj01/wp-content/plugins/mv-translations/views/mv-translations_metabox.php
@@ -1,3 +1,13 @@
+<?php
+
+global $wpdb;
+$query   = $wpdb->prepare(
+	"SELECT * FROM $wpdb->translationmeta
+    WHERE translation_id = %d",
+	$post->ID
+);
+$results = $wpdb->get_results( $query, ARRAY_A );
+?>
 <table class="form-table mv-translations-metabox">
     <!-- Nonce -->
     <input type="hidden" name="mv_translations_nonce" value="<?php echo wp_create_nonce( 'mv_translations_nonce' ); ?>">
@@ -7,9 +17,13 @@
         </th>
         <td>
             <select name="mv_translations_transliteration" id="mv_translations_transliteration">
-                <option value="Yes"><?php esc_html_e( 'Yes', 'mv-translations' ) ?></option>
+                <option value="Yes" <?php if ( isset( $results[0]['meta_value'] ) ) {
+					selected( $results[0]['meta_value'], 'Yes' );
+				} ?>><?php esc_html_e( 'Yes', 'mv-translations' ) ?></option>
                 ';
-                <option value="No"><?php esc_html_e( 'No', 'mv-translations' ) ?></option>
+                <option value="No" <?php if ( isset( $results[0]['meta_value'] ) ) {
+					selected( $results[0]['meta_value'], 'No' );
+				} ?>><?php esc_html_e( 'No', 'mv-translations' ) ?></option>
                 ';
             </select>
         </td>
@@ -24,7 +38,7 @@
                     name="mv_translations_video_url"
                     id="mv_translations_video_url"
                     class="regular-text video-url"
-                    value=""
+                    value="<?php echo ( isset( $results[1]['meta_value'] ) ) ? esc_url( $results[1]['meta_value'] ) : ""; ?>"
             >
         </td>
     </tr>
