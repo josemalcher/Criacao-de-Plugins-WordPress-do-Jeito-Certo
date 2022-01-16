5e338f7af9cbb69b54d10397200e619dfb034332
José Malcher Jr <contato@josemalcher.net>
2022-01-16 09:22:11 -0300

    90 Criando a view para o shortcode (parte 9)

--- wp-proj01/wp-content/plugins/mv-translations/views/mv-translations_shortcode.php
+++ wp-proj01/wp-content/plugins/mv-translations/views/mv-translations_shortcode.php
@@ -135,29 +135,39 @@ if ( isset( $_POST['submitted'] ) ) {
 		$current_user->ID
 	);
 	$results = $wpdb->get_results( $q );
-	var_dump( $results );
-	?>
-    <table>
-        <caption><?php esc_html_e( 'Your Translations', 'mv-translations' ); ?></caption>
-        <thead>
-        <tr>
-            <th><?php esc_html_e( 'Date', 'mv-translations' ); ?></th>
-            <th><?php esc_html_e( 'Title', 'mv-translations' ); ?></th>
-            <th><?php esc_html_e( 'Transliteration', 'mv-translations' ); ?></th>
-            <th><?php esc_html_e( 'Edit?', 'mv-translations' ); ?></th>
-            <th><?php esc_html_e( 'Delete?', 'mv-translations' ); ?></th>
-            <th><?php esc_html_e( 'Status', 'mv-translations' ); ?></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>Date</td>
-            <td>Title</td>
-            <td>Transliteraton</td>
-            <td>Edit</td>
-            <td>Delete</td>
-            <td>Status</td>
-        </tr>
-        </tbody>
-    </table>
+
+	if ( $wpdb->num_rows ):
+		?>
+        <table>
+            <caption><?php esc_html_e( 'Your Translations', 'mv-translations' ); ?></caption>
+            <thead>
+            <tr>
+                <th><?php esc_html_e( 'Date', 'mv-translations' ); ?></th>
+                <th><?php esc_html_e( 'Title', 'mv-translations' ); ?></th>
+                <th><?php esc_html_e( 'Transliteration', 'mv-translations' ); ?></th>
+                <th><?php esc_html_e( 'Edit?', 'mv-translations' ); ?></th>
+                <th><?php esc_html_e( 'Delete?', 'mv-translations' ); ?></th>
+                <th><?php esc_html_e( 'Status', 'mv-translations' ); ?></th>
+            </tr>
+            </thead>
+            <tbody>
+			<?php foreach ( $results as $result ): ?>
+                <tr>
+                    <td><?php echo esc_html( date( 'M/d/Y', strtotime( $result->post_date ) ) ); ?></td>
+                    <td><?php echo esc_html( $result->post_title ); ?></td>
+                    <td><?php echo $result->meta_value == 'Yes' ? esc_html__( 'Yes', 'mv-translations' ) : esc_html__( 'No', 'mv-translations' ); ?></td>
+					<?php $edit_post = add_query_arg( 'post', $result->ID, home_url( '/edit-translation' ) ); ?>
+                    <td>
+                        <a href="<?php echo esc_url( $edit_post ); ?>"><?php esc_html_e( 'Edit', 'mv-translations' ); ?></a>
+                    </td>
+                    <td>
+                        <a onclick="return confirm( 'Are you sure you want to delete post: <?php echo $result->post_title ?>?' )"
+                           href="<?php echo get_delete_post_link( $result->ID, "", true ); ?>"><?php esc_html_e( 'Delete', 'mv-translations' ); ?></a>
+                    </td>
+                    <td><?php echo $result->post_status == 'publish' ? esc_html__( 'Published', 'mv-translations' ) : esc_html__( 'Pending', 'mv-translations' ); ?></td>
+                </tr>
+			<?php endforeach; ?>
+            </tbody>
+        </table>
+	<?php endif; ?>
 </div>
